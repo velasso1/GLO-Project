c04254e8284a57a0aeb76fe833e6e42d3612ccf4
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2023-01-10 19:12:39 +0300

    выполнение задания

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,7 +1,51 @@
 'use strict';
 
 const validation = () => {
-    console.log('validation');
+    const calcSquare = document.querySelector('.calc-square');
+    const calcCount = document.querySelector('.calc-count');
+    const calcDay = document.querySelector('.calc-day');
+
+    const formName = document.querySelectorAll('.form-name');
+    const formEmail = document.querySelectorAll('.form-email');
+    const formPhone = document.querySelectorAll('.form-phone');
+    const mess = document.querySelector('.mess');
+
+    calcSquare.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/\D/, '');
+    });
+
+    calcCount.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/\D/, '');
+    });
+
+    calcDay.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/\D/, '');
+    });
+
+
+    formPhone.forEach(function (form) {
+        form.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9()-]/, '');
+        });
+    });
+
+    formEmail.forEach(function (form) {
+        form.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^a-zA-Z0-9@-_.!~*']/, '');
+        });
+    });
+
+    formName.forEach(function (form) {
+        form.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
+        });
+    });
+
+    mess.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, '');
+    });
+
+
 };
 
 export default validation;
