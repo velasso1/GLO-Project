9b235d02ec12b4b361394e3f847d1af820185ec7
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2023-01-10 20:53:07 +0300

    выполнение задания

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,16 +3,20 @@
 const menu = () => {
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
-    const closeBtn = menu.querySelector('.close-btn');
-    const menuItems = menu.querySelectorAll('ul>li>a');
 
     const toggleMenu = function () {
         menu.classList.toggle('active-menu');
     };
 
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', toggleMenu));
+    menu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('close-btn')) {
+            toggleMenu();
+        } else if (e.target.matches('a')) {
+            toggleMenu();
+        }
+    });
+
     menuBtn.addEventListener('click', toggleMenu);
-    closeBtn.addEventListener('click', toggleMenu);
 };
 
 export default menu;
