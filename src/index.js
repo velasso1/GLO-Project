f122c9b2d40f1d8006f3be90d724a60ff7c69e01
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2023-01-14 15:59:55 +0300

    добавление файла sendForm.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import validation from './modules/validation';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
 import calc from './modules/calc';
+import sendForm from './modules/sendForm';
 
 
 timer('19 january 2023');
@@ -16,3 +17,4 @@ validation();
 tabs();
 slider();
 calc(100);
+sendForm();
---
+++ src/modules/sendForm.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const sendForm = () => {
+    console.log('form');
+};
+
+export default sendForm;
